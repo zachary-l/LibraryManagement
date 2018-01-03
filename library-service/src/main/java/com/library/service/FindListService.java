af19ff5b2f68df3c5f1185946c205c808415ba57
zachary <1456932921@qq.com>
2018-01-03 13:49:56 +0800

    1/3

--- library-service/src/main/java/com/library/service/FindListService.java
+++ library-service/src/main/java/com/library/service/FindListService.java
@@ -2,15 +2,17 @@ package com.library.service;
 
 import com.library.dao.FindListDao;
 import com.library.dto.PageBean;
+import com.library.exception.FlowException;
 import org.framework.beans.annotation.Component;
 import org.framework.beans.annotation.Inject;
 
 import java.util.List;
 import java.util.Map;
 @Component("findBooksListService")
-public class FindBooksListService {
+public class FindListService {
     @Inject("findListDao")
     private FindListDao findListDao;
+    //显示列表
     public PageBean findBooksList(int currentPage) {
         PageBean pageBean = new PageBean();
         pageBean.setCurrentPage(currentPage);
@@ -20,5 +22,14 @@ public class FindBooksListService {
         List<Map<String, Object>> list = findListDao.findList(pageBean.getFirstResult(), pageBean.getMaxResult());
         pageBean.setList(list);
         return pageBean;
+    }
+    //删除罚金记录
+    public PageBean deleteFindList(int rfId,int currentPage){
+        int row = 0;
+        row = findListDao.deleteFindList(rfId);
+        if(row==0){
+            throw new FlowException("删除罚金记录失败");
+        }
+        return findBooksList(currentPage);
     }
 }
